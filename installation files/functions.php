6b363a370b8a96b0c4589f42744a4ed65970c9c0
Joseph E <joseph@thedealio.org>
2017-06-29 09:41:57 -0600

    Installation Files
    Files that help make the transformation complete.

---
+++ installation files/functions.php
@@ -0,0 +1,9 @@
+Add to Functions PHP
+
+add_theme_support( 'post-thumbnails' );
+        add_image_size( 'eborall-thumb', 737, 250, true ); 
+		add_image_size( 'portfolio-thumb', 600, 300, true ); 
+        add_image_size( 'portfolio-thumb-mobile', 450, 250, true ); 
+		add_image_size( 'portfolio-thumb_small', 400, 269, true ); 
+		add_image_size( 'portfolio-widget', 100, 100, true );
+        add_image_size( 'eborall_post', 780, 340, true );
